3cbacac71c6ed6f487e7958d3e679680ab189114
Ananya Ghosh <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-12-06 11:11:38 +0530

    Add files via upload

---
+++ Game Database app using Public API/src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,24 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: '18f6068328274741bc40bbcb96c2d703',
+      }
+    });
+    return next.handle(req);
+  }
+}
